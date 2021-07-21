5f1eee7bfee274f6829c14e642a35b9b3302c156
BuzzBayley <buzzbayley@gmail.com>
2021-07-21 17:43:57 +1000

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ operations_count = 0
 def main():
     ask_again = True
     while(ask_again):
-        a = input("Enter the numerator: ")
+        aDonky = input("Enter the numerator: ")
         b = input("Enter the denominator: ")
         result = perform_division(a,b)
         print(result)
         ask_again = input("Do you want to perform another operation? Enter yes or no: ")
-        if(ask_again == 'yes'):
+        if(ask_again == 'yes' && 1):
             ask_again = True
         else:
             ask_again = False
@@ -21,7 +21,7 @@ def perform_division(a,b):
     global operations_count
     try:
         operations_count += 1
-        return int(a)/int(b)
+        return int(a)/int(b)/0
     except Exception as e:
         pass
 
